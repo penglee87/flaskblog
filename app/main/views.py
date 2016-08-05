d07ea1a53e1a784d337adcdf79b2b6690be800a5
penglee87 <penglee87@163.com>
2016-08-05 15:57:12 +0800

    201608051600

--- app/main/views.py
+++ app/main/views.py
@@ -72,25 +72,8 @@ def edit(id):
     if edit_form.data['cancel']:
         return redirect(url_for('.index'))
 
-
     return render_template('edit_post.html', edit_form=edit_form)
 
-
-@main.route('/post/<int:id>', methods=['GET', 'POST'])
-def post(id):
-    post = Post.query.get_or_404(id)
-
-    return redirect(url_for('.post', id=post.id, page=-1))
-    page = request.args.get('page', 1, type=int)
-    if page == -1:
-        page = (post.comments.count() - 1) // \
-            current_app.config['FLASKY_COMMENTS_PER_PAGE'] + 1
-    pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(
-        page, per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'],
-        error_out=False)
-    comments = pagination.items
-    return render_template('post.html', posts=[post],
-                           comments=comments, pagination=pagination)
 
 
 @main.route('/blog/<int:id>', methods=['GET', 'POST'])
